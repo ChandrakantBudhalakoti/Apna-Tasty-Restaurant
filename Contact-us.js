6d11d116f8b39ef4b2c9b4082223a24a069dd1d5
Chandrakant Budhalakoti <121392931+ChandrakantBudhalakoti@users.noreply.github.com>
2023-11-27 16:51:06 +0530

    Add files via upload

--- Contact-us.js
+++ Contact-us.js
@@ -1,39 +1,83 @@
-// Recieve mail in Gmail
-function emailSend() {
+function validateForm() {
+  // Retrieve input values
   var name = document.getElementById("name").value;
   var email = document.getElementById("email").value;
   var phone = document.getElementById("phone").value;
   var message = document.getElementById("message").value;
 
+  // Check if inputs are empty
   if (!name || !email || !phone || !message) {
-    swal('Oops...', 'All field are required!', 'error');
+    swal("Oops...", "Somthing Wrong!!!", "error");
     return false;
   }
 
+  // Validate email using regex
+  var re = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+  if (!re.test(email)) {
+    document.getElementById("emailError").classList.remove("d-none");
+    return false;
+  } else {
+    document.getElementById("emailError").classList.add("d-none");
+  }
+
+  // Validate phone number using regex
+  var rePhone = /^\d{10}$/;
+  if (!rePhone.test(phone)) {
+    document.getElementById("phoneError").classList.remove("d-none");
+    return false;
+  } else {
+    document.getElementById("phoneError").classList.add("d-none");
+  }
+
+  return true;
+}
+
+function emailSend() {
+  // Check form validation
+  if (!validateForm()) {
+    return false;
+  }
+
+  // Get form values
+  var name = document.getElementById("name").value;
+  var email = document.getElementById("email").value;
+  var phone = document.getElementById("phone").value;
+  var message = document.getElementById("message").value;
+
+  // Create message body
   var messageBody =
-    "Name: " +
+    "<b>Name: </b>" +
     name +
-    "<br/> Email " +
+    "<br> <b>Email: </b>" +
     email +
-    "<br/> Phone: " +
+    "<br> <b>Phone: </b>" +
     phone +
-    "<br/> Message: " +
+    "<br> <b>Message: </b>" +
     message;
+
+  // Send email using Email.js
   Email.send({
     SecureToken: "98a3bf57-15d6-4ede-85d6-46a9ca29a8b6",
     To: "chandrakantbudhalakoti189@gmail.com",
     From: "chetan.budhalakoti@gmail.com",
-    Subject: "New Message from Apna Tasty Restaurant",
+    Subject: "Apna Tasty Restaurant - New Enquiry",
     Body: messageBody,
-  }).then((message) => {
-    if (message == "OK") {
-      swal(
-        "Thank You !!!",
-        "We recieved your message we try to respond soon...",
-        "success"
-      );
-    } else {
-      swal("Error", "somthing happen !", "error");
-    }
+  }).then(function (message) {
+    // Show success alert
+    swal(
+      "Thank You !!!",
+      "We recieved your message we try to respond soon...",
+      "success"
+    );
   });
+
+  return false;
 }
+
+// Checks for form submission event
+document.getElementById("contactForm").addEventListener("submit", function (event) {
+   // Prevents the default form submission behavior  
+  event.preventDefault();
+      // Sends the email
+    emailSend();
+  });
